2e63e7f1e402e64f82d9f6222d8b8c57e6115187
Prasad Bhaskar Pawar <pbpawar2004@gmail.com>
2025-01-23 22:56:36 +0530

    Create Clone List with Next and Random.py

---
+++ Clone List with Next and Random.py
@@ -0,0 +1,62 @@
+You are given a special linked list with n nodes where each node has two pointers a next pointer that points to the next node of the singly linked list, and a random pointer that points to the random node of the linked list.
+
+Construct a copy of this linked list. The copy should consist of the same number of new nodes, where each new node has the value corresponding to its original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list, such that it also represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+
+Return the head of the copied linked list.
+
+NOTE : Original linked list should remain unchanged.
+
+Examples:
+
+Input: head = [[1, 3], [3, 3], [5, NULL], [9, 3]] 
+      
+Output: head = [[1, 3], [3, 3], [5, NULL], [9, 3]] 
+Explanation: 
+Node 1 points to Node 2 as its NEXT and Node 3 as its RANDOM.
+Node 2 points to Node 3 as its NEXT and Node 3 as its RANDOM.
+Node 3 points to Node 4 as its NEXT and NULL as its RANDOM.
+Node 4 points to NULL as its NEXT and Node 3 as its RANDOM.
+Input: head = [[1, 3], [2, 1], [3, 5], [4, 3], [5, 2]]
+  
+ 
+Output: head = [[1, 3], [2, 1], [3, 5], [4, 3], [5, 2]]
+Explanation: 
+Node 1 points to Node 2 as its NEXT and Node 3 as its RANDOM.
+Node 2 points to Node 3 as its NEXT and Node 1 as its RANDOM.
+Node 3 points to Node 4 as its NEXT and Node 5 as its RANDOM.
+Node 4 points to Node 5 as its NEXT and Node 3 as its RANDOM.
+Node 5 points to NULL as its NEXT and Node 2 as its RANDOM.
+Input: head = [[7, NULL], [7, NULL]]
+Output: head = [[7, NULL], [7, NULL]]
+Explanation: 
+Node 1 points to Node 2 as its NEXT and NULL as its RANDOM.
+Node 2 points to NULL as its NEXT and NULL as its RANDOM.
+Constraints:
+1 <= n <= 100
+0 <= node->data <= 1000
+class Solution {
+    public Node cloneLinkedList(Node head) {
+        // code here
+        Map <Node,Node> t1=new HashMap<>();
+        Node temp = head;
+        Node res1=new Node(temp.data);
+        Node res=res1;
+        t1.put(temp,res);
+        temp=temp.next;
+        while(temp!=null){
+            res.next=new Node(temp.data);
+            res=res.next;
+            t1.put(temp,res);
+            temp=temp.next;
+        }
+        temp=head;
+        res=res1;
+        while(temp!=null){
+            res.random=t1.get(temp.random);
+            res=res.next;
+            temp=temp.next;
+        }
+        //System.out.println(t1);
+        return res1;
+    }
+}
